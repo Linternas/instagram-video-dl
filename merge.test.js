c09bbdfa878238756e43e32b1f78ce1a82757cbd
linternas <kmg20000215@gmail.com>
2020-08-22 23:29:59 +0900

    video audio 파일 합치기

--- merge.test.js
+++ merge.test.js
@@ -1,10 +1,30 @@
-const getusermedia = require('getusermedia')
-const screenRecord = require('screen-record')
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+ffmpeg.setFfmpegPath(ffmpegPath);
 
-getusermedia({ video: true, audio: true }, (err, webcamStream) => {
-  screenRecord(window, (err, sourceId, constraints) => {
-    getusermedia(constraints, (err, screenStream) => {
-      // We now have 2 streams: webcamStream, screenStream
-    })
+let proc = new ffmpeg();
+
+proc.addInput('public/video.mp4')
+  .addInput('public/audio.mp4')
+  .on('start', function (ffmpegCommand) {
+    console.log('start');
+    /// log something maybe
   })
-})
+  .on('progress', function (progress) {
+    // console.log('progress');
+    console.log(progress.timemark);
+    /// do stuff with progress data if you want
+  })
+  .on('end', function () {
+    console.log('end')
+    /// encoding is complete, so callback or move on at this point
+  })
+  .on('error', function (error) {
+    console.log(error)
+    /// error handling
+  })
+  .seekInput('45:00')
+  .size('640x480')
+
+  .save('out.mp4')
+  .run();
